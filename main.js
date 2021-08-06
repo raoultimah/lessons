c13a3ae34283100c709c83a3062d0bcc5e424970
Raoulred <75142066+Raoulred@users.noreply.github.com>
2021-08-06 14:56:51 +0100

    created index.html and main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+// const Num1 = window.prompt('Enter first Number');
+// const Num2 = window.prompt('Enter second Number');
+// const Ans = Num1 % Num2;
+// window.alert(Ans);
+
+
+const name = window.prompt("Enter name/email");
+const password = window.prompt("Enter Password");
+
+if(name == "Raoul" && password == "password"){
+    window.alert("You're Logged In");
+}else{
+    window.alert("Invalid Name or Password");
+}
+
+
+
+// let i ;
+// for (i = 1; i < 21; i++){
+//     if(i >=5 && i % 5 == 0){
+//         window.alert('Wandas');
+//     }else{
+//         window.alert(i);
+//     }
+// }
+
+
+// let Num1 = parseFloat(window.prompt('Enter first Number'));
+// let Num2 = parseFloat(window.prompt('Enter second Number')); 
+// const operator = window.prompt("Enter the Operator to Use \n 1 for Addition \n 2 for Subtraction \n 3 for Multiplication \n 4 for Division ");
+
+// if(operator == 1){
+//     Ans = Num2 + Num1;
+// }else if(operator == 2){
+//     Ans = Num1 - Num2;
+// }else if(operator == 3){
+//     Ans = Num1 * Num2;
+// }else if(operator == 4){
+//     Ans = Num1 / Num2;
+// }else{
+//     window.alert("Choose a correct Operator")
+// }
+// window.alert(Ans)
